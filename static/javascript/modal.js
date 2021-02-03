4a19d49cb9713348c8e490b311eebbfb68cc7960
Sander <chikunotani223@gmail.com>
2021-02-03 18:55:13 +0900

    部屋を閉めるモーダルの作成

--- static/javascript/modal.js
+++ static/javascript/modal.js
@@ -1,46 +1,77 @@
-function newRoom() {
-  const newRoomButton = document.getElementById("new-room-btn");
+function modal() {
   const overlay = document.querySelector(".overlay");
-  const newRoomModal = document.querySelector(".new-room-modal")
-  const newRoomClose = document.querySelector(".new-room-close")
 
-  // 部屋を作るモーダルを開く処理
-  newRoomButton.addEventListener("click", () => {
-    overlay.classList.add("open");
-    newRoomModal.classList.add("open");
-  });
 
-  // 部屋を作るモーダルを閉じる処理
-  newRoomClose.addEventListener("click", () => {
-    overlay.classList.remove("open");
-    newRoomModal.classList.remove("open");
-  });
-  overlay.addEventListener("click", () => {
-    overlay.classList.remove("open");
-    newRoomModal.classList.remove("open");
-  });
+  if(!document.URL.match(/room/)){
+    // 部屋を作るモーダル
+    const newRoomButton = document.getElementById("new-room-btn");
+    const newRoomModal = document.querySelector(".new-room-modal");
+    const newRoomClose = document.querySelector(".new-room-close");
 
-  const joinRoom = document.querySelectorAll(".join-room");
-  const joinRoomModal = document.querySelector(".join-room-modal");
-  const joinRoomClose = document.querySelector(".join-room-close");
-
-  // 部屋に入るモーダルを開く処理
-  for(let i = 0; i < joinRoom.length; i++){
-    joinRoom[i].addEventListener("click", () => {
+    // 部屋を作るモーダルを開く処理
+    newRoomButton.addEventListener("click", () => {
       overlay.classList.add("open");
-      joinRoomModal.classList.add("open");
+      newRoomModal.classList.add("open");
+    });
+
+    // 部屋を作るモーダルを閉じる処理
+    newRoomClose.addEventListener("click", () => {
+      overlay.classList.remove("open");
+      newRoomModal.classList.remove("open");
+    });
+    overlay.addEventListener("click", () => {
+      overlay.classList.remove("open");
+      newRoomModal.classList.remove("open");
+    });
+
+
+    // 部屋に入るモーダル
+    const joinRoom = document.querySelectorAll(".join-room");
+    const joinRoomModal = document.querySelector(".join-room-modal");
+    const joinRoomClose = document.querySelector(".join-room-close");
+
+    // 部屋に入るモーダルを開く処理
+    for(let i = 0; i < joinRoom.length; i++){
+      joinRoom[i].addEventListener("click", () => {
+        overlay.classList.add("open");
+        joinRoomModal.classList.add("open");
+      });
+    }
+
+    // 部屋に入るモーダルを閉じる処理
+    joinRoomClose.addEventListener("click", () => {
+      overlay.classList.remove("open");
+      joinRoomModal.classList.remove("open");
+    });
+    overlay.addEventListener("click", () => {
+      overlay.classList.remove("open");
+      joinRoomModal.classList.remove("open");
     });
   }
 
-  // 部屋に入るモーダルを閉じる処理
-  joinRoomClose.addEventListener("click", () => {
-    overlay.classList.remove("open");
-    joinRoomModal.classList.remove("open");
-  });
-  overlay.addEventListener("click", () => {
-    overlay.classList.remove("open");
-    joinRoomModal.classList.remove("open");
-  });
+  
+  // 部屋を閉めるモーダル
+  if(document.URL.match(/room/)){
+    const closeRoomButton = document.getElementById("chat-room-close");
+    const closeRoomModal = document.querySelector(".close-room-modal");
+    const closeRoomClose = document.querySelector(".close-room-close");
+
+    // 部屋を閉めるモーダルを開く処理
+    closeRoomButton.addEventListener("click", () => {
+      overlay.classList.add("open");
+      closeRoomModal.classList.add("open");
+    });
+
+    // 部屋を閉めるモーダルを閉じる処理
+    closeRoomClose.addEventListener("click", () => {
+      overlay.classList.remove("open");
+      closeRoomModal.classList.remove("open");
+    });
+    overlay.addEventListener("click", () => {
+      overlay.classList.remove("open");
+      closeRoomModal.classList.remove("open");
+    });
+  }
 }
 
-window.addEventListener("load", newRoom);
+window.addEventListener("load", modal);
