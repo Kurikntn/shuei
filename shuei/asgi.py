be105612123b46234972814e0b01571388c9581c
WatanabeR <watanaber@tracks.co.jp>
2021-02-06 14:20:18 +0900

    organize asgi code

--- shuei/asgi.py
+++ shuei/asgi.py
@@ -1,26 +1,15 @@
-"""
-ASGI config for shuei project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
-"""
+from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+import chat.urls
 
 import os
 
-from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shuei.settings')
 
 # application = get_asgi_application()
 django_asgi_app = get_asgi_application()
-
-from channels.routing import ProtocolTypeRouter
-
-from channels.routing import URLRouter
-from channels.auth import AuthMiddlewareStack
-import chat.urls
 
 application = ProtocolTypeRouter( {
     'http': get_asgi_application(),
