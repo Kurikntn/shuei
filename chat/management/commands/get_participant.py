267171f33d573ccd6d72696af32a2a8447ce7d98
WatanabeR <watanaber@tracks.co.jp>
2021-02-13 15:04:20 +0900

    add start_watchdog to get_participant

---
+++ chat/management/commands/get_participant.py
@@ -0,0 +1,24 @@
+from django.core.management.base import BaseCommand, CommandError
+from django.conf import settings
+from chat.models import Room
+
+import datetime
+import redis
+
+JST = datetime.timezone(datetime.timedelta(hours=+9), 'JST')
+
+class Command(BaseCommand):
+
+  def handle(self, *args, **options):
+    now = datetime.datetime.now(datetime.timezone.utc)
+    rooms = Room.objects.all()
+    redis_cli = redis.Redis(host=settings.ALLOWED_HOSTS[0], port=settings.REDIS_PORT)
+    for room in rooms:
+      redis_room = redis_cli.zrange("asgi:group:room_" + str(room.id), 0, -1)    
+      room.participants_num = len(redis_room)
+      room.save()
+      #部屋作成から10分経過　かつ　人数0なら削除
+      if(room.participants_num == 0 and 
+        ((room.at + datetime.timedelta(minutes=10)) < now)
+      ):
+        room.delete()
--- chat/models.py
+++ chat/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class Room(models.Model):
   name = models.CharField(max_length=50)
   capacity = models.IntegerField(default=3, verbose_name="制限人数")
+  participants_num = models.IntegerField(default=0, verbose_name="参加人数")
   time = models.IntegerField(default=8, verbose_name="制限時間")
-  at = models.DateTimeField(verbose_name='作成日時')
+  at = models.DateTimeField(default=timezone.now, verbose_name='作成日時')
