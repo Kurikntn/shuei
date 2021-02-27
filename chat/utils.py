c8ce086589fd9e79ae88027f75ac911258117ef0
WatanabeR <watanaber@tracks.co.jp>
2021-02-27 16:39:42 +0900

    fix redis connect

--- chat/utils.py
+++ chat/utils.py
@@ -5,8 +5,11 @@ import os
 
 
 def connect():
-  return redis.from_url(
-    url=os.environ.get(settings.REDIS_URL),
-    decode_responses=True,
-  )
+  if setting.REDIS_URL:
+    return redis.from_url(
+      url=os.environ.get(settings.REDIS_URL),
+      decode_responses=True,
+    )
+  else:
+    return redis.Redis(host=settings.ALLOWED_HOSTS[0], port=settings.REDIS_PORT)
 
