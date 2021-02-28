0fac3d21014adbfe94c59408a82f4dbdd1f98d5c
Sander <chikunotani223@gmail.com>
2021-03-01 01:43:45 +0900

    部屋作成モーダルのcss更新

--- static/javascript/new_room.js
+++ static/javascript/new_room.js
@@ -6,7 +6,11 @@ roomNameInput.oninput = roomName;
 function roomName(e) {
   if(e.target.value == ""){
     newRoomSubmit.disabled = true;
+    newRoomSubmit.style.backgroundColor = "#aaa";
+    newRoomSubmit.style.cursor = "default";
   } else {
     newRoomSubmit.disabled = false;
+    newRoomSubmit.style.backgroundColor = "#1919fa";
+    newRoomSubmit.style.cursor = "pointer";
   }
 }
