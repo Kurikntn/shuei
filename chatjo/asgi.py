33bec2a25e1a18bb637d4a958be614576d887169
chatjo_dev <developer@ip-172-31-38-187.ap-northeast-1.compute.internal>
2021-04-02 01:10:25 +0900

    fix asgi.py

--- chatjo/asgi.py
+++ chatjo/asgi.py
@@ -1,16 +1,21 @@
+import os
+from django.conf.urls import url
 from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chatjo.settings')
+django_asgi_app = get_asgi_application()
+
+
+import django
+django.setup()
+
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import chat.routing
-import os
 
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chatjo.settings')
-
-# application = get_asgi_application()
-django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter( {
-    'http': get_asgi_application(),
+    'http': django_asgi_app,
     'websocket': AuthMiddlewareStack( URLRouter( chat.routing.websocket_urlpatterns ) ),
 } )
